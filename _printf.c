36b48d214c4a38ddf94576bcec5ed1ef0096454e
wilstermanz <4917@holbertonschool.com>
2022-06-29 11:55:04 -0700

    Created some of the required base files and a directory for main.c test files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Prints a string with formatted data
+ * @format - String containing data variables
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
