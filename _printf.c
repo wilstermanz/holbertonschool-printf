4cc867e6b6ebb70655ca662fc713eca012dd3bb0
wilstermanz <4917@holbertonschool.com>
2022-06-30 08:59:21 -0700

    Added pseudocode to _printf file

--- _printf.c
+++ _printf.c
@@ -8,5 +8,22 @@
 
 int _printf(const char *format, ...)
 {
+	/* Declare variables and struct */
+
+	/* Allocate 1024 bytes for output string */
+
+	/* va_start */
+
+	/* Iterate through format string */
+
+	/* Check for % symbol */
+
+	/*
+	 * If format[i] == '%', check next character
+	 * and send to corresponding function.
+	 */
+
+	write(1, &output, count);
+	return (count);
 
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
