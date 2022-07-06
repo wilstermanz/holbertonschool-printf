8f680e80bfaa8459af01f0e052af95ddb042e385
wilstermanz <4917@holbertonschool.com>
2022-07-06 10:48:40 -0700

    Added "/x" to beginning of identifier

--- print_STRING.c
+++ print_STRING.c
@@ -1,6 +1,38 @@
 #include "main.h"
 
 /**
+ * hex_convert - converts decimal to hex
+ * @output:
+ * @count:
+ */
+
+void hex_convert(char *output, int *count, int num)
+{
+	char *hexconverter = "0123456789ABCDEF";
+	int decimals[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	int hex[2];
+	int i, j;
+
+	for (i = 0; i < 2; i++)
+	{
+		hex[i] = num % 16;
+		num /= 16;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		for (j = 0; j <= 15; j++)
+		{
+			if (hex[i] == decimals[j])
+			{
+				output[*count] = hexconverter[j];
+				(*count)++;
+				break;
+			}
+		}
+	}
+}
+
+/**
  * print_STRING - prints string including unprintable characters
  * @ap: argument pointer
  * @output: string
@@ -10,10 +42,7 @@
 void print_STRING(va_list ap, char *output, int *count)
 {
 	char *str = va_arg(ap, char *);
-	int i, j, k, num;
-	char *hexconverter = "0123456789ABCDEF";
-	int decimals[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-	int hex[2];
+	int i, num;
 
 	if (str == NULL)
 		str = "(null)";
@@ -28,23 +57,11 @@ void print_STRING(va_list ap, char *output, int *count)
 		else
 		{
 			num = str[i];
-			for (j = 0; j < 2; j++)
-			{
-				hex[j] = num % 16;
-				num /= 16;
-			}
-			for (j -= 1; j >= 0; j--)
-			{
-				for (k = 0; k <= 15; k++)
-				{
-					if (hex[j] == decimals[k])
-					{
-						output[*count] = hexconverter[k];
-						(*count)++;
-						break;
-					}
-				}
-			}
+			output[*count] = 92;
+			(*count)++;
+			output[*count] = 'x';
+			(*count)++;
+			hex_convert(output, count, num);
 		}
 	}
 }
